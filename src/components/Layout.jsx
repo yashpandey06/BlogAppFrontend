b2b04c3a2e00096b72ca095eb7b7e9ec095cd306
Yash Pandey <pandeyyash7052@gmail.com>
2023-06-12 19:15:12 +0530

    final commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,18 @@ import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
 import axios from 'axios';
 import { useEffect, useState } from "react";
+import Cookies from "js-cookie"
 const Layout = () => {
   const [username, setUsername] = useState("");
 
 
   useEffect(() => {
+    const token = Cookies.get('token');
     axios.get('https://blogapp-em6i.onrender.com/auth', {
       withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+      },
     })
       .then((response) => {
         
