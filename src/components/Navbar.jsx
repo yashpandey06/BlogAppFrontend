a561d3644d3911b8e1e8983d8dda18ab9ac728a0
Yash Pandey <pandeyyash7052@gmail.com>
2023-06-12 19:02:06 +0530

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,15 @@
-
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import BlurOnOutlinedIcon from '@mui/icons-material/BlurOnOutlined';
 
-
-// eslint-disable-next-line react/prop-types
 const Navbar = ({ username }) => {
-  console.log("navbar +" ,username)
   const deleteCookie = (name) => {
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
   };
-  function Logout() {
+
+  const handleLogout = () => {
     deleteCookie('token');
     window.location.reload();
-  }
+  };
 
   return (
     <header className="py-5 px-8 md:px-12 flex bg-fixed items-center justify-between">
@@ -22,20 +19,23 @@ const Navbar = ({ username }) => {
         </div>
         <span className="text-black text-3xl font-bold font-signature">Bluff</span>
       </Link>
-      {username ?
-        <div className="flex items-center gap-4 text-md  ">
+
+      {username ? (
+        <div className="flex items-center gap-4 text-md">
           <button>{username}</button>
-          <button className=' bg-black rounded-xl  p-2 text-white' onClick={Logout}>logout</button>
+          <button className="bg-black rounded-xl p-2 text-white" onClick={handleLogout}>
+            Logout
+          </button>
         </div>
-        : <Link to="/login" className=' md:flex bg-black rounded-xl  p-2 text-white'>
-          <div className="lg:flex items-center gap-4 text-md  ">
+      ) : (
+        <Link to="/login" className="md:flex bg-black rounded-xl p-2 text-white">
+          <div className="lg:flex items-center gap-4 text-md">
             <button>Sign in</button>
           </div>
-        </Link>}
-
-
+        </Link>
+      )}
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
